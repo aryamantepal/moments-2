b2e0a08126a93b1aa28eb4520bbe0540ebfa65cb
aryamantepal <aryamandora@gmail.com>
2026-01-08 17:00:46 +0400

    hashing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-export default function Home() {
+import { neon } from '@neondatabase/serverless';
+import * as bcrypt from 'bcrypt';
+
+export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+
+    const sql = neon(process.env.DATABASE_URL!);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await sql`
+      INSERT INTO users (email)
+      VALUES (${email})
+    `;
+  }
 
   return (
-    <div>
-      Welcome!
-    </ div>
+    <form action={create}>
+      <input type="email" name="email" placeholder="Enter your email" />
+      <input type="password" name="password" placeholder="Enter your password" />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
