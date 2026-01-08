7a0a47091cc285c3a18ba38c000fcbd5ae68336e
aryamantepal <aryamandora@gmail.com>
2026-01-08 16:15:42 +0400

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <div onClick={() => setCount(count + 1)} className="flex items-center justify-center h-screen text-6xl cursor-pointer select-none">
+    <div>
       {count}
     </ div>
   );
