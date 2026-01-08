f1676a621d84aab64e75fd2e4d816696a3eaa173
aryamantepal <aryamandora@gmail.com>
2026-01-08 17:35:47 +0400

    display all users

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-export default function Page() {
+'use server';
+
+import { neon } from '@neondatabase/serverless';
+
+export async function getUsers() {
+  const sql = neon(process.env.DATABASE_URL!);
+
+  // Tell TypeScript exactly what each row looks like
+  const users = await sql`
+    SELECT *
+    FROM users
+  `;
+
+  return users;
+}
+
+export default async function Page() {
+  const users = await getUsers();
   return (
     <div>
-      Welcome!
+      <ol className="list-decimal list-inside font-(family-name:--font-geist-sans) text-[#333333]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.email}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
