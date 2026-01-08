0ff80ffeb7bb82309f50dc1722c6accaedff4a88
aryamantepal <aryamandora@gmail.com>
2026-01-08 16:16:01 +0400

    more cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-"use client"
-import Image from "next/image";
-import { useState } from "react";
-
 export default function Home() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
-      {count}
+      Welcome!
     </ div>
   );
 }
