a1386fc2ab4e94402fa1899282a8d8adb8fca4b5
aryamantepal <aryamandora@gmail.com>
2026-01-09 18:45:25 +0400

    something like this

--- app/moments/page.tsx
+++ app/moments/page.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next";
 import { cookies } from 'next/headers';
 import { neon } from '@neondatabase/serverless';
 import { redirect } from 'next/navigation';
@@ -13,26 +12,14 @@ export default async function Page() {
     }
 
     const sql = neon(process.env.DATABASE_URL!);
-    const users = await sql`
-  select email from users where id = ${userId} LIMIT 1
+    const moments = await sql`
+  select * from moments
   `
-    const user = users[0];
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="flex flex-col items-center gap-4">
-                <h1>Welcome, {user.email}</h1>
-                <form action={signOut}>
-                    <button type="submit">Sign Out</button>
-                </form>
             </div>
         </div>
     );
 
-}
-
-async function signOut() {
-    'use server';
-    const cookieStore = await cookies();
-    cookieStore.delete('user_id');
-    redirect('/sign-in');
 }
