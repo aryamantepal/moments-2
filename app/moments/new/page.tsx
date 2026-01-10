33290b92ec7c58f52b280a0cef6d4a64e6fde243
aryamantepal <aryamandora@gmail.com>
2026-01-10 23:10:36 +0400

    setup for new moments

--- app/moments/new/page.tsx
+++ app/moments/new/page.tsx
@@ -1,4 +1,12 @@
+import Form from "next/form";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { put } from '@vercel/blob';
+
 export default function Page() {
+    async function create(formData: FormData) {
+        'use server';
+    }
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="flex flex-col items-center gap-4">
