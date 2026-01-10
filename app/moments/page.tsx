39dea74ff837ce52ab20e6e3d3fd25c6b994daa2
aryamantepal <aryamandora@gmail.com>
2026-01-11 01:49:23 +0400

    line

--- app/moments/page.tsx
+++ app/moments/page.tsx
@@ -18,6 +18,7 @@ export default async function Moments() {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="flex flex-col items-center gap-4">
+                Moments main display
             </div>
         </div>
     );
