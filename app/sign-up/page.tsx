d939e717470a2a82de38cf3ba73087539faa9acf
aryamantepal <aryamandora@gmail.com>
2026-01-12 21:15:14 +0400

    sign up signs you in

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import * as bcrypt from 'bcrypt';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
 export default function Page() {
     async function create(formData: FormData) {
@@ -16,6 +18,18 @@ export default function Page() {
       INSERT INTO users (email, password_hash, name)
       VALUES (${email}, ${hashedPassword}, ${username})
     `;
+        const users = await sql`SELECT * FROM users where email = ${email} LIMIT 1`;
+        const user = users[0];
+
+        const cookieStore = await cookies();
+        cookieStore.set('user_id', user.id.toString(), {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'lax',
+            maxAge: 60 * 60 * 24 * 7 // 1 week
+        });
+
+        redirect('/');
     }
 
     return (
